54b38387633f79561bac0d219c800ac4004e8606
Paulo Henrique Correa dos Santos <paulo.santos@engdb.com.br>
2017-04-11 16:03:26 -0300

    Classe teste de performance (List X Set)

---
+++ Alura/src/br/com/empresa/banco/TestaPerformance.java
@@ -0,0 +1,40 @@
+package br.com.empresa.banco;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashSet;
+
+public class TestaPerformance {
+
+	public static void main(String[] args) {
+
+		System.out.println("Iniciando..................");
+		
+		Collection<Integer> teste = new HashSet<Integer>();
+		long inicio = System.currentTimeMillis();
+		
+		int total = 30000;
+		
+		System.out.println("1° for");
+		for(int i = 0; i < total; i++){
+			teste.add(i);
+		}
+		long fimForHum = System.currentTimeMillis();
+		long totalFimForHum = fimForHum - inicio;
+		System.out.println("Tempo de inserção: " + totalFimForHum);
+		
+		System.out.println("2° for");
+		long inicioBusca = System.currentTimeMillis();
+		for(int i = 0; i < total; i++){
+			teste.contains(i);
+		}
+		long fimFroDois = System.currentTimeMillis();
+		long totalFimForDois = fimFroDois - inicioBusca;
+		System.out.println("Tempo de busca: " + totalFimForDois);
+		
+		long fim = System.currentTimeMillis();
+		long tempo = fim - inicio;
+		System.out.println("Tempo total gasto: " + tempo);
+	}
+
+}
