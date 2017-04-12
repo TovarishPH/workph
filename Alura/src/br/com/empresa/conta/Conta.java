306b06d22bfabbf7b3c7fbfba2c185fc3cabd89e
Paulo Henrique Correa dos Santos <paulo.santos@engdb.com.br>
2017-04-12 14:16:59 -0300

    Implementação de hashcode nan classe Conta

--- Alura/src/br/com/empresa/conta/Conta.java
+++ Alura/src/br/com/empresa/conta/Conta.java
@@ -33,11 +33,32 @@ public abstract class Conta implements Comparable<Conta>{
 		return "este objeto é uma conta, com saldo R$" + this.saldo;
 	}
 	
+	/* (non-Javadoc)
+	 * @see java.lang.Object#hashCode()
+	 */
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + numero;
+		return result;
+	}
+
+	/* (non-Javadoc)
+	 * @see java.lang.Object#equals(java.lang.Object)
+	 */
 	@Override
 	public boolean equals(Object obj) {
-		Conta outraConta = (Conta) obj;
-		
-		return this.numero == outraConta.numero && this.nome.equals(outraConta.nome);
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Conta other = (Conta) obj;
+		if (numero != other.numero)
+			return false;
+		return true;
 	}
 
 	public int getNumero() {
