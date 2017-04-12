09ae2a82682062ec6b52e72819f9c01e2700eb1d
Paulo Henrique Correa dos Santos <paulo.santos@engdb.com.br>
2017-04-12 14:11:47 -0300

    Classe de teste de revserse de um TreeSet

---
+++ Alura/src/br/com/empresa/banco/TreesetDescrescente.java
@@ -0,0 +1,21 @@
+package br.com.empresa.banco;
+
+import java.util.Collections;
+import java.util.Set;
+import java.util.SortedSet;
+import java.util.TreeSet;
+
+public class TreesetDescrescente {
+
+	public static void main(String[] args) {
+		
+		Set<Integer> nums = new TreeSet<Integer>();
+		
+		for(int i = 0; i <= 1000; i++){
+			nums.add(i);
+		}
+		
+		// Aplicar a ordem reversa do Set
+	}
+
+}
