3f3f13f412a99779b56fa0b29c8e3783aeafa9db
Paulo Henrique Correa dos Santos <paulo.santos@engdb.com.br>
2017-04-20 18:01:15 -0300

    Classe de teste do TreeSet descendente

--- Alura/src/br/com/empresa/banco/TreesetDescrescente.java
+++ Alura/src/br/com/empresa/banco/TreesetDescrescente.java
@@ -1,21 +1,29 @@
 package br.com.empresa.banco;
 
-import java.util.Collections;
-import java.util.Set;
-import java.util.SortedSet;
+import java.util.NavigableSet;
 import java.util.TreeSet;
 
 public class TreesetDescrescente {
 
 	public static void main(String[] args) {
 		
-		Set<Integer> nums = new TreeSet<Integer>();
+		TreeSet<Integer> nums = new TreeSet<Integer>();
 		
 		for(int i = 0; i <= 1000; i++){
 			nums.add(i);
 		}
 		
 		// Aplicar a ordem reversa do Set
+		
+//		NavigableSet<Integer> descNums= nums.descendingSet();
+//		
+//		for (Integer integer : descNums) {
+//			System.out.println(integer);
+//		}
+		
+		for (Integer integer : nums.descendingSet()) {
+			System.out.println(integer);
+		}
 	}
 
 }
