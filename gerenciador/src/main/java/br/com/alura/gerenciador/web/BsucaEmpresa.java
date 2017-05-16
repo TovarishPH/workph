54686678118efcccd5d6eac28b4a44f1f7edd28b
Paulo Henrique Correa dos Santos <paulo.santos@engdb.com.br>
2017-05-16 12:12:11 -0300

    Visualizando instância, inicialização e destruição da servlet

--- gerenciador/src/main/java/br/com/alura/gerenciador/web/BsucaEmpresa.java
+++ gerenciador/src/main/java/br/com/alura/gerenciador/web/BsucaEmpresa.java
@@ -15,27 +15,52 @@ import br.com.alura.gerenciador.dao.EmpresaDAO;
 
 @WebServlet(urlPatterns = "/busca")
 public class BsucaEmpresa extends HttpServlet {
+	
+	public BsucaEmpresa() {
+		System.out.println("Instanciando uma Servlet do tipo BsucaEmpresa "
+				+ this);
+	}
+	
+	@Override
+	public void init() throws ServletException {
+		super.init();
+		System.out.println("Inicializando a servlet " + this);
+	}
 
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp)
 			throws ServletException, IOException {
-		
+
 		PrintWriter writer = resp.getWriter();
 		writer.println("<html>");
 		writer.println("<body>");
 		writer.println("<p>Resultado da Busca: </p></br>");
-		
+
 		String filtro = req.getParameter("filtro");
 		
-		Collection<Empresa> empresas = new EmpresaDAO().buscaPorSimilaridade(filtro);
-		
+		try{
+			Thread.sleep(10000);
+		}catch (InterruptedException e){
+			e.printStackTrace();
+		}
+
+		Collection<Empresa> empresas = new EmpresaDAO()
+				.buscaPorSimilaridade(filtro);
+
 		writer.println("<ul>");
 		for (Empresa empresa : empresas) {
-			writer.println("<li>" + empresa.getId() + " : " + empresa.getNome() + "</li>");
+			writer.println("<li>" + empresa.getId() + " : " + empresa.getNome()
+					+ "</li>");
 		}
 		writer.println("</ul>");
-		
+
 		writer.println("</body>");
 		writer.println("</html>");
+	}
+	
+	@Override
+	public void destroy() {
+		super.destroy();
+		System.out.println("Destruindo a servlet " + this);
 	}
 }
