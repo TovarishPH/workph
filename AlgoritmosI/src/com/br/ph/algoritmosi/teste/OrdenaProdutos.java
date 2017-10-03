475421dda7811ba28c0ffae9e24d242debc24b01
PH - Secundary Device <phcorreacontato@gmail.com>
2017-10-03 11:24:53 -0300

    Ordenação de produtos

--- AlgoritmosI/src/com/br/ph/algoritmosi/teste/OrdenaProdutos.java
+++ AlgoritmosI/src/com/br/ph/algoritmosi/teste/OrdenaProdutos.java
@@ -12,33 +12,32 @@ public class OrdenaProdutos {
 		produtos[2] = new Produto("Brasilia", 16000);
 		produtos[3] = new Produto("Smart", 46000);
 		produtos[4] = new Produto("Fusca", 17000);
-
-		for (int i = 0; i < produtos.length; i++){
-			int menor = buscaMenorPreco(produtos, i);
-			Produto produtoAtual = produtos[i];
-			Produto produtoMaisBarato = produtos[menor];
-			produtos[i] = produtoMaisBarato;
-			produtos[menor] = produtoAtual;
+		
+		
+		
+		for(int atual = 0; atual < produtos.length; atual++){
+			
+			int maisBarato = buscaProdutoMaisBarato(produtos, atual, produtos.length);
+			
+			Produto produtoAtual = produtos[atual];
+			Produto produtoMaisBarato = produtos[maisBarato];
+			produtos[atual] = produtoMaisBarato;
+			produtos[maisBarato] = produtoAtual;
 		}
 		
-		for (Produto produto : produtos) {
-			System.out.println(produto.getNome() + " " + produto.getPreco());
+		for (int i = 0; i < produtos.length; i++) {
+			System.out.println(produtos[i].getNome() + ", R$" + produtos[i].getPreco());
 		}
 		
-//		System.out.println("Produto mais barato: "
-//				+ produtos[maisBarato].getNome() + ", valor: "
-//				+ produtos[maisBarato].getPreco() + ".");
 	}
 
-	private static int buscaMenorPreco(Produto[] produtos, int inicio) {
-		int maisBarato = 0;
-		for (int atual = inicio; atual < produtos.length; atual++) {
-			if (produtos[atual].getPreco() < produtos[maisBarato].getPreco()) {
+	private static int buscaProdutoMaisBarato(Produto[] produtos, int inicio, int fim) {
+		int maisBarato = inicio;
+		for (int atual = inicio; atual < fim; atual ++){
+			if(produtos[atual].getPreco() < produtos[maisBarato].getPreco()){
 				maisBarato = atual;
 			}
-			
 		}
 		return maisBarato;
 	}
-
 }
