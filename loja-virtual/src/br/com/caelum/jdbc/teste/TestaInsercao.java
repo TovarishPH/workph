b9a4fba566410917a47ff732946af07197f3ed01
PH - Secundary Device <phcorreacontato@gmail.com>
2017-10-17 12:25:09 -0200

    Insercao utilizando PreparedStatement no lugar de Statement(evitando sql injection)

--- loja-virtual/src/br/com/caelum/jdbc/teste/TestaInsercao.java
+++ loja-virtual/src/br/com/caelum/jdbc/teste/TestaInsercao.java
@@ -2,6 +2,7 @@ package br.com.caelum.jdbc.teste;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
@@ -12,21 +13,35 @@ public class TestaInsercao {
 
 	public static void main(String[] args) throws SQLException {
 		
-		//Connection con = DriverManager.getConnection("jdbc:mysql://localhost/loja_virtual", "root", "root");
+//		Connection con = DataBase.getConnection();
+//		Statement stmt = con.createStatement();
+//		boolean resultado = stmt.execute("insert into Produto (nome, descricao) values ('notebook', 'notebook i5')", stmt.RETURN_GENERATED_KEYS);
+//		System.out.println(resultado);
+		
 		Connection con = DataBase.getConnection();
-		Statement stmt = con.createStatement();
-		boolean resultado = stmt.execute("insert into Produto (nome, descricao) values ('notebook', 'notebook i5')", stmt.RETURN_GENERATED_KEYS);
+		String sql = "insert into Produto (nome, descricao) values (?, ?)";
+		PreparedStatement pstmt = con.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
+		adiciona("mochila", "mochila de alpinismo", pstmt);
+		adiciona("televisor", "tv led 42''", pstmt);
+		
+		pstmt.close();
+		con.close();
+	}
+
+	private static void adiciona(String nome, String descricao,
+			PreparedStatement pstmt) throws SQLException {
+		pstmt.setString(1, nome);
+		pstmt.setString(2, descricao);
+		boolean resultado = pstmt.execute();
+		
 		System.out.println(resultado);
 		
-		ResultSet rs = stmt.getGeneratedKeys();
+		ResultSet rs = pstmt.getGeneratedKeys();
 		
 		while (rs.next()) {
 			String id = rs.getString(1);
 			System.out.println("ID: " + id + " gerado");
 		}
-		
 		rs.close();
-		stmt.close();
-		con.close();
 	}
 }
