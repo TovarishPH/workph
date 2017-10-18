f6c2404ab30ea88448cd04ff18f218dada5a3475
PH - Secundary Device <phcorreacontato@gmail.com>
2017-10-18 14:41:15 -0200

    Separacao de responsabilidade no acesso ao banco(DAO)

---
+++ loja-virtual/src/br/com/caelum/jdbc/conexao/ProdutosDAO.java
@@ -0,0 +1,64 @@
+package br.com.caelum.jdbc.conexao;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+
+import br.com.caelum.jdbc.modelo.Produto;
+
+public class ProdutosDAO {
+
+	private Connection con;
+
+	public ProdutosDAO(Connection con) {
+		this.con = con;
+	}
+
+	public void salva(Produto produto) throws SQLException {
+		String sql = "insert into Produto(nome, descricao) values (?, ?)";
+		try (PreparedStatement ps = con.prepareStatement(sql,
+				Statement.RETURN_GENERATED_KEYS)) {
+
+			ps.setString(1, produto.getNome());
+			ps.setString(2, produto.getDescricao());
+			boolean resultado = ps.execute();
+			con.commit();
+			System.out.println(resultado);
+
+			ResultSet rs = ps.getGeneratedKeys();
+
+			while (rs.next()) {
+				int id = rs.getInt(1);
+				produto.setId(id);
+				System.out.println("ID " + id + " inserido.");
+			}
+		}
+	}
+
+	public List<Produto> lista() throws SQLException {
+		List<Produto> produtos = new ArrayList<Produto>();
+		
+		String sql = "select * from Produto";
+		
+		try(PreparedStatement ps = con.prepareStatement(sql)){
+			boolean resultado = ps.execute();
+			System.out.println(resultado);
+			
+			try(ResultSet rs = ps.getResultSet()){
+				
+				while(rs.next()){
+					Produto prod = new Produto(rs.getString(2), rs.getString(3));
+					prod.setId(rs.getInt(1));
+					produtos.add(prod);
+					//System.out.println("Produto: [" + prod.getId() + "] " + prod.getNome() + " - " + prod.getDescricao());
+				}
+			}
+		}
+		
+		return produtos;
+	}
+}
--- loja-virtual/src/br/com/caelum/jdbc/teste/TesteComProduto.java
+++ loja-virtual/src/br/com/caelum/jdbc/teste/TesteComProduto.java
@@ -1,11 +1,10 @@
 package br.com.caelum.jdbc.teste;
 
 import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.sql.Statement;
+import java.util.List;
 
+import br.com.caelum.jdbc.conexao.ProdutosDAO;
 import br.com.caelum.jdbc.conexao.ConnectionPool;
 import br.com.caelum.jdbc.modelo.Produto;
 
@@ -13,38 +12,17 @@ public class TesteComProduto {
 
 	public static void main(String[] args) throws SQLException {
 
-		Produto mesa = new Produto("mesa", "mesa de jantar");
+		Produto mesa = new Produto("mesa azul", "mesa de jantar");
 
-		ConnectionPool cp = new ConnectionPool();
-
-		try (Connection con = cp.getConnection()) {
+		try (Connection con = new ConnectionPool().getConnection()) {
+			ProdutosDAO produtosDAO = new ProdutosDAO(con);
 			con.setAutoCommit(false);
-			String sql = "insert into Produto(nome, descricao) values (?, ?)";
-			try (PreparedStatement ps = con.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS)) {
-				adiciona(mesa, ps);
-				con.commit();
-				System.out.println("O prodtudo inserido com sucesso: " + mesa);
-			} catch (Exception e) {
-				e.printStackTrace();
-				con.rollback();
-				System.out.println("Deu ruim, malandro!");
+			produtosDAO.salva(mesa);
+			
+			List<Produto> produtos = produtosDAO.lista();
+			for (Produto produto : produtos) {
+				System.out.println("Existe o produto: " + produto);
 			}
-		}
-	}
-
-	private static void adiciona(Produto produto, PreparedStatement ps)
-			throws SQLException {
-		ps.setString(1, produto.getNome());
-		ps.setString(2, produto.getDescricao());
-		boolean resultado = ps.execute();
-		System.out.println(resultado);
-
-		ResultSet rs = ps.getGeneratedKeys();
-
-		while (rs.next()) {
-			int id = rs.getInt(1);
-			produto.setId(id);
-			System.out.println("ID " + id + " inserido.");
 		}
 	}
 }
