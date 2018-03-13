d66bd64484d9940063e171ae593d332106410795
PHCorrea <phcorreacontato@gmail.com>
2018-03-12 22:19:39 -0300

    Projeto JSF - Autor CRUD

--- jsf-livraria/src/br/com/ph/livraria/bean/AutorBean.java
+++ jsf-livraria/src/br/com/ph/livraria/bean/AutorBean.java
@@ -1,12 +1,17 @@
 package br.com.ph.livraria.bean;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
 
 import br.com.ph.livraria.dao.DAO;
 import br.com.ph.livraria.model.Autor;
 import br.com.ph.livraria.util.RedirectView;
 
 @ManagedBean
+@ViewScoped
 public class AutorBean {
 
 	private Autor autor = new Autor();
@@ -14,11 +19,30 @@ public class AutorBean {
 	public Autor getAutor() {
 		return autor;
 	}
+	
+	public List<Autor> getAutores(){
+		return new DAO(Autor.class).listaTodos();
+	}
+	
+	public void removerAutor(Autor autor){
+		System.out.println("Removendo autor " + autor.getNome());
+		new DAO<Autor>(Autor.class).remove(autor);
+		this.getAutores().remove(autor);
+	}
+	
+	public void alterarAutor(Autor autor){
+		System.out.println("Carregando autor " + autor.getNome());
+		this.autor = autor;
+	}
 
 	public RedirectView gravar() {
 		System.out.println("Gravando autor " + this.autor.getNome());
 
-		new DAO<Autor>(Autor.class).adiciona(this.autor);
+		if(this.autor.getId() == null){
+			new DAO<Autor>(Autor.class).adiciona(this.autor);
+		}else {
+			new DAO<Autor>(Autor.class).atualiza(this.autor);
+		}
 		
 		autor = new Autor();
 		
